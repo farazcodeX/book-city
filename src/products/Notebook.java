f29ae0bc474ae9a71228442f3fc46bcc3b0365e3
faraz <farazn8564@gmail.com>
2025-04-25 22:49:19 +0330

    notebook class

--- src/products/Notebook.java
+++ src/products/Notebook.java
@@ -1,5 +1,30 @@
 package products;
 
-public class Notebook {
+public class Notebook extends Product{
+
+    private int pageCount;
+    private boolean isHardCover;
+    public Notebook(String name, double price, String id, int pages, boolean isHard) {
+        super(name, price, id);
+        this.isHardCover = isHard;
+        setPageCount(pages);   
+    }
+    public void setPageCount(int count) {
+        if(count > 1) {
+            this.pageCount = count;
+        }
+    }
+
+    public String toString() {
+        return super.toString() + " PageCount : " + pageCount + " hard cover ? " + isHardCover;
+    }
+
+    @Override
+    protected String generated() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generated'");
+    }
+
+
     
 }
